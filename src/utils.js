0ad2c52b7da4580f22ea6f305bb85c301247635b
Yuliya <yuliya55122@gmail.com>
2021-07-27 18:48:22 +0300

    fetchitemsfromFirebase

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,9 @@
-const addItemsToFirebase = () => {
-  // fetch POST
-  // url: https://koifish-21741-default-rtdb.europe-west1.firebasedatabase.app/hot_rolls.json
-  // body: массив
-  // headers: ?
-
-  // гугли как отправлять POST запросы с помощью fetch.
-
-  // fetch возвращает promise (вспоминай как обрабатывать ответ)
+export const addItemsToFirebase = (item, rollId) => {
+  fetch(`https://koifish-21741-default-rtdb.europe-west1.firebasedatabase.app/${rollId}.json`, {
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  })
 };
-
-
-addItemsToFirebase();
